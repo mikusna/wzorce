b6bb5728fa4228575633c80407c318c3d8bdc48f
mikusna <mil.deb99@gmail.com>
2024-10-07 20:46:23 +0200

    FactoryMethod

---
+++ 02_Factory/Program.cs
@@ -0,0 +1,30 @@
+﻿using Factory;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Garnizon garnizon = new Garnizon();
+
+        //utworzenie tablicy wojowników i dodanie odpowiednich obiektów
+        Wojownik[] wojownik = new Wojownik[]
+        {
+            garnizon.WyszkolWojownika("Piechur"),
+            garnizon.WyszkolWojownika("Piechur"),
+            garnizon.WyszkolWojownika("Piechur"),
+            garnizon.WyszkolWojownika("Konny"),
+            garnizon.WyszkolWojownika("Konny"),
+            garnizon.WyszkolWojownika("Konny"),
+            garnizon.WyszkolWojownika("Strzelec"),
+            garnizon.WyszkolWojownika("Strzelec"),
+            garnizon.WyszkolWojownika("Strzelec"),
+            garnizon.WyszkolWojownika("Strzelec")
+        };
+
+        //dla każdego wojownika wyświetlenie jego profesji
+        foreach (var woj in wojownik)
+        {
+            Console.WriteLine(woj.Profesja);
+        }
+    }
+}
