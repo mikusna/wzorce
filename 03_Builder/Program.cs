56aaec70e40c7d159b901ec190f1215a5a043789
mikusna <mil.deb99@gmail.com>
2024-10-08 19:57:32 +0200

    Add files via upload

---
+++ 03_Builder/Program.cs
@@ -0,0 +1,27 @@
+﻿using WarriorBuilderZadanie;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Garnizon garnizon = new Garnizon();
+
+        // Dodanie 2 piechurów
+        WarriorBuilder piechurBuilder = new PiechurBuilder();
+        garnizon.WyszkolWojownika(piechurBuilder);
+        garnizon.WyszkolWojownika(piechurBuilder);
+
+        // Dodanie 2 strzelców
+        WarriorBuilder strzelecBuilder = new StrzelecBuilder();
+        garnizon.WyszkolWojownika(strzelecBuilder);
+        garnizon.WyszkolWojownika(strzelecBuilder);
+
+        // Dodanie 2 konnych
+        WarriorBuilder konnyBuilder = new KonnyBuilder();
+        garnizon.WyszkolWojownika(konnyBuilder);
+        garnizon.WyszkolWojownika(konnyBuilder);
+
+        // Wyświetlenie wojowników
+        garnizon.PokazWojownikow();
+    }
+}
