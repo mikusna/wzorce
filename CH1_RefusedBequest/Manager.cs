2977c046053f982e104aee09577febef9c3d9ab3
mikusna <mil.deb99@gmail.com>
2024-10-24 21:10:09 +0200

    Add files via upload

---
+++ CH1_RefusedBequest/Manager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Wprowadź interfejs IEmployee do klasy Manager i przenieś do niej tylko te metody, które są używane.
+
+namespace RefusedBequest
+{
+    public class Manager : Employee
+    {
+        public void ManageTeam() 
+        {
+            Console.WriteLine($"{Name} is managing the team.");
+        }
+    }
+}
---
+++ CH1_RefusedBequest/Program.cs
@@ -0,0 +1,16 @@
+﻿using RefusedBequest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Manager manager = new Manager {Name = "Alicja", Position = "Manager" };
+        manager.Work();
+        manager.ManageTeam();
+
+        Employee employee = new Employee {Name = "Jacek", Position = "Developer" };
+        employee.Work();
+        employee.AttendMeeting();
+        Console.ReadKey();
+    }
+}
