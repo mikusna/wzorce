5c0eb438f58ec18782956f884c38d9bf851c1bf2
mikusna <mil.deb99@gmail.com>
2024-10-24 19:19:59 +0200

    Add files via upload

---
+++ CH1_LongParameterList/ProductInfo.cs
@@ -0,0 +1,32 @@
+﻿namespace Product
+{
+    public class LongParameterList
+    {
+        public void RegisterProduct(ProductInfo product)
+        {
+            // Rejestracja produktu
+            Console.WriteLine($"Product: {product.ProductName}, Category: {product.Category}, Price: {product.Price:C}, Stock: {product.Stock}, Supplier: {product.SupplierName}, Contact: {product.SupplierContact}");
+        }
+    }
+
+    public class ProductInfo // Przeniesiono klasę ProductInfo poza LongParameterList
+    {
+        public string ProductName { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string SupplierName { get; set; }
+        public string SupplierContact { get; set; }
+
+        // Konstruktor klasy ProductInfo
+        public ProductInfo(string productName, string category, decimal price, int stock, string supplierName, string supplierContact)
+        {
+            ProductName = productName;
+            Category = category;
+            Price = price;
+            Stock = stock;
+            SupplierName = supplierName;
+            SupplierContact = supplierContact;
+        }
+    }
+}
