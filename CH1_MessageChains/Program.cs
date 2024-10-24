b00afab68bc7bea0c9381732c9abd0e6d992f0e2
mikusna <mil.deb99@gmail.com>
2024-10-24 20:09:31 +0200

    Add files via upload

---
+++ CH1_MessageChains/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car();
+        //wywołanie nowej metody GetCylinderSize
+        string cylinderSize = car.GetCylinderSize();
+        Console.WriteLine($"Cylinder size: {cylinderSize}");
+        Console.ReadKey();
+    }
+}
+
+
+
+public class Car
+{
+    public Engine GetEngine()
+    {
+        return new Engine();
+    }
+
+    //nowa metoda GetCylinderSize() w klasie Car
+    public string GetCylinderSize()
+    {
+        return GetEngine().GetCylinder().GetSize();
+    }
+}
+
+public class Engine
+{
+    public Cylinder GetCylinder()
+    {
+        return new Cylinder();
+    }
+}
+
+public class Cylinder
+{
+    public string GetSize()
+    {
+        return "2.0L";
+    }
+}
