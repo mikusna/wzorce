6c22496cc56f5b86884c990304d3a9713dbd92ae
mikusna <mil.deb99@gmail.com>
2024-10-24 21:37:42 +0200

    Add files via upload

---
+++ CH1_TemporaryField/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+
+public class InvoiceGenerator
+{
+    public void GenerateInvoice()
+    {
+        //tymczasowe pola zmienione na lokalne zmienne
+        int invoiceNumber = 12345;
+        PdfWriter pdfWriter = new PdfWriter($"Invoice_{invoiceNumber}.pdf");
+
+        // Generowanie faktury
+        pdfWriter.Write("Invoice Content");
+        pdfWriter.Close();
+    }
+
+    public void OtherMethod()
+    {
+        // Metoda, która nie korzysta z tymczasowych zmiennych
+    }
+}
+
+
