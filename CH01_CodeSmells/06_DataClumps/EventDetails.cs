86c7f5e802c4ea78ec55956610ce72ec46966417
mikusna <mil.deb99@gmail.com>
2024-11-10 20:14:00 +0100

    Adding projects to repo

---
+++ CH01_CodeSmells/06_DataClumps/EventDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Utwórz nową klasę EventDetails, która będzie zawierała pola eventName, eventDate, location.
+namespace DataClumps
+{
+    public class EventDetails
+    {
+        public string EventName { get; set; }
+        public DateTime EventDate { get; set; }
+        public string Location { get; set; }
+
+        public EventDetails(string eventName, DateTime eventDate, string location)
+        {
+            EventName = eventName;
+            EventDate = eventDate;
+            Location = location;
+        }
+    }
+
+}
---
+++ CH01_CodeSmells/06_DataClumps/Program.cs
@@ -0,0 +1,16 @@
+﻿using DataClumps;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        EventDetails eventDetails = new EventDetails("First date", DateTime.Now, "Pool");
+        RegisterEvent(eventDetails);
+        Console.ReadKey();
+    }
+
+    public static void RegisterEvent(EventDetails eventDetails)
+    {
+        Console.WriteLine($"Event: {eventDetails.EventName}, Date: {eventDetails.EventDate}, Location: {eventDetails.Location}");
+    }
+}
