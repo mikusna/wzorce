6982479a9a8c525663e5b06e1562f1e224a093ed
mikusna <mil.deb99@gmail.com>
2024-11-15 19:32:01 +0100

    Adding projects to repo

---
+++ CH06_NetDesignPatterns/06_Proxy/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IUserInterface publicResource = new PublicFile();
+            IUserInterface privateResource = new Proxy(new RestrictedFile(), "123");
+
+            while (true)
+            {
+                Console.WriteLine("Select file:");
+                Console.WriteLine("1. Public file.");
+                Console.WriteLine("2. Private file.");
+                Console.WriteLine("0. Exit");
+                
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        publicResource.AccessFile();
+                        break;
+                    case "2":
+                        privateResource.AccessFile();
+                        break;
+                    case "0":
+                        Console.WriteLine("Exit.");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Try again.");
+                        break;
+                }
+
+                
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ CH06_NetDesignPatterns/06_Proxy/Proxy.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    public interface IUserInterface
+    {
+        void AccessFile();
+    }
+
+    //obiekt dostępny bez ograniczeń
+    public class PublicFile : IUserInterface
+    {
+        public void AccessFile()
+        {
+            Console.WriteLine("Public File. Press enter to go back to main menu.");
+        }
+    }
+
+    //obiekt zastrzeżony
+    public class RestrictedFile : IUserInterface
+    {
+        public void AccessFile()
+        {
+            Console.WriteLine("Restriced file. Press enter to go back to main menu.");
+        }
+    }
+
+    //proxy - kontrola dostępu do plików poprzez sprawdzenie czy hasło zwrócone zostało poprawnie
+    public class Proxy : IUserInterface
+    {
+        private readonly IUserInterface _file;
+        private readonly string _password;
+
+        public Proxy(IUserInterface file, string password)
+        {
+            _file = file;
+            _password = password;
+        }
+
+        public void AccessFile()
+        {
+            Console.Write("Password: ");
+            string input = Console.ReadLine();
+
+            if (input == _password)
+            {
+                _file.AccessFile();
+            }
+            else
+            {
+                Console.WriteLine("Access denied. Incorrect password.");
+            }
+        }
+    }
+
+
+
+}
+
