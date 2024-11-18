317356917091a2d00e3ec99d2daebabc0031eed2
mikusna <mil.deb99@gmail.com>
2024-11-18 22:05:10 +0100

    Adding projects to repo

---
+++ CH06_NetDesignPatterns/08_Bridge/ConsoleInterface.cs
@@ -0,0 +1,17 @@
+﻿using Bridge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    public class ConsoleInterface : Interface
+    {
+        void Interface.DisplayMenu()
+        {
+            Console.WriteLine("Console Interface");
+        }
+    }
+}
---
+++ CH06_NetDesignPatterns/08_Bridge/Program.cs
@@ -0,0 +1,17 @@
+﻿using Bridge;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var linux = new Linux();
+        linux.userInterface = new ConsoleInterface();
+        linux.DisplayMenu();
+
+        linux.userInterface = new GraphicInterface();
+        linux.DisplayMenu();
+
+
+        Console.ReadKey();
+    }
+}
