454a514fec3c7f9c97bc43b0527b2319f12fcfb1
mikusna <mil.deb99@gmail.com>
2025-01-06 14:30:41 +0100

    Add project to repo

---
+++ CH01_CodeSmells/07_LongMessageChain/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class School
+{
+    public string GetTeacherName()
+    {
+        Classroom classroom = new Classroom();
+        return classroom.GetTeacher().GetName();
+    }
+}
+
+public class Classroom
+{
+    public Teacher GetTeacher()
+    {
+        return new Teacher();
+    }
+}
+
+public class Teacher
+{
+    public string GetName()
+    {
+        return "John Smith";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        School school = new School();
+        string teacherName = school.GetTeacherName();
+        Console.WriteLine(teacherName);
+        Console.ReadKey();
+    }
+}
