26ff300e51ef83bcd49207a5b91f9dd6ad5562ce
mikusna <mil.deb99@gmail.com>
2025-01-06 14:12:22 +0100

    Add project to repo

---
+++ CH06_NetDesignPatterns/10_Composite/Program.cs
@@ -0,0 +1,42 @@
+﻿using _10_Composite;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dish kimchi = new Dish("Kimchi", 15.00m);
+        Dish bibimbap = new Dish("Bibimbap", 25.00m);
+        Dish bulgogi = new Dish("Bulgogi", 30.00m);
+        Dish japchae = new Dish("Japchae", 28.00m);
+        Dish tteokbokki = new Dish("Tteokbokki", 18.00m);
+        Dish bingsu = new Dish("Bingsu", 22.00m);
+        Dish hotteok = new Dish("Hotteok", 12.00m);
+        Dish yakgwa = new Dish("Yakgwa", 10.00m);
+
+        Category appetizers = new Category("Przystawki:");
+        Category mainDishes = new Category("Dania główne:");
+        Category desserts = new Category("Desery:");
+
+        appetizers.AddItem(kimchi);
+        appetizers.AddItem(tteokbokki);
+
+        mainDishes.AddItem(bibimbap);
+        mainDishes.AddItem(bulgogi);
+        mainDishes.AddItem(japchae);
+
+        desserts.AddItem(bingsu);
+        desserts.AddItem(hotteok);
+        desserts.AddItem(yakgwa);
+
+        Category menu = new Category("Menu");
+
+        menu.AddItem(appetizers);
+        menu.AddItem(mainDishes);
+        menu.AddItem(desserts);
+
+        Console.WriteLine("Restauracja koreańska");
+        menu.Display(0);
+
+        Console.ReadKey();
+    }
+}
---
+++ CH06_NetDesignPatterns/10_Composite/RestaurantMenu.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Composite
+{
+    public class Dish
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public Dish(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public void Display(int depth)
+        {
+            Console.WriteLine(new string(' ', depth * 2) + $" {Name} - {Price} PLN");
+        }
+    }
+
+    public class Category
+    {
+        public string Name { get; set; }
+        public List<object> Items { get; set; }
+
+        public Category(string name)
+        {
+            Name = name;
+            Items = new List<object>();
+        }
+
+        public void AddItem(object item)
+        {
+            Items.Add(item);
+        }
+
+        public void Display(int depth)
+        {
+            Console.WriteLine(new string(' ', depth * 2) + Name);
+            foreach (var item in Items)
+            {
+                if (item is Category category)
+                {
+                    category.Display(depth + 1);
+                }
+                else if (item is Dish dish)
+                {
+                    dish.Display(depth + 1);
+                }
+            }
+        }
+    }
+}
